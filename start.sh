0af7553f385eea341a9d0cde1cc6ab0922c08715
Henrique <henrique.dias@gruponewway.com.br>
2025-08-02 15:37:37 -0300

    chore: adding start.sh

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e  # Encerra o script se algum comando falhar
+
+echo "🔧 Iniciando serviços: MySQL, PostgreSQL e Trino..."
+docker-compose up -d
+echo "✅ Serviços iniciados com sucesso."
+
+# Define função para encerrar containers ao receber sinal de interrupção
+cleanup() {
+  echo ""
+  echo "🛑 Encerrando serviços Docker..."
+  docker-compose down
+  echo "✅ Serviços finalizados."
+  exit 0
+}
+
+# Captura sinais de interrupção (Ctrl+C) ou erro
+trap cleanup INT TERM ERR
+
+sleep 10  # Aguarda 10 segundos para garantir que os serviços sejam iniciados corretamente
+# Inicia a aplicação Node
+echo "🚀 Iniciando aplicação Node.js..."
+{
+  cd app
+  npm install
+  npm start
+}
